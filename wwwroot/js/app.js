3bcace485c6b186b9d61d09996e991e8d305d8cf
baschingz <36448854+baschingz@users.noreply.github.com>
2018-08-06 17:33:01 +0700

    Initial commit

---
+++ wwwroot/js/app.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module("myapp", []);
+var url_string = document.URL;
+var url = new URL(url_string);
+var access_token = url.searchParams.get("access_token");
+if (access_token != null) {
+    localStorage.token = access_token;
+}
+
